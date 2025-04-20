ea688629699696cbfdb9f77cec31a023537db5a3
Johannes <142503761+j551n-ncloud@users.noreply.github.com>
2025-04-20 15:57:45 +0200

    fixed

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,9 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    hmr: {
+      overlay: false, // ✅ Disable the error overlay
+    },
   },
   plugins: [
     react(),
